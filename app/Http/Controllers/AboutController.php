e71e3c715f5ae0219f20e4992b59d34e6cb64594
premlamsal <premlamsal2@gmail.com>
2020-05-06 20:07:39 +0545

    fixes about us edit error form admin panel

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -2,35 +2,48 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\About;
+use Illuminate\Http\Request;
 
 class AboutController extends Controller
 {
-    
+
     public function index()
     {
-        $about=About::all();
-        return view('admin.addAbout')->with('about',$about);
+        $about = About::all();
+        return view('admin.addAbout')->with('about', $about);
     }
 
-  
     public function edit($id)
     {
-        $about=About::find($id);
-        return view('admin.addAbout')->with('about',$about);
+        $about = About::find($id);
+        if ($about) {
+
+            return view('admin.addAbout')->with('about', $about);
+
+        } else {
+            $about             = new About();
+            $about->about_body = "Sample text. Please change from admin section.";
+
+            if ($about->save()) {
+                return view('admin.addAbout')->with('about', $about);
+            } else {
+                echo "There is not about content in database. Failed creating it.";
+            }
+
+        }
+
     }
 
     public function update(Request $request, $id)
     {
         $this->validate($request, [
-            'about_body' => 'required'
+            'about_body' => 'required',
         ]);
-        $about=About::find($id);
-        $about->about_body=$request->input('about_body');
+        $about             = About::find($id);
+        $about->about_body = $request->input('about_body');
         $about->save();
         return back();
     }
 
-   
 }
